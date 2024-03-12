63ece33f861a88c311f450263b4ed885bd4ba044
Vladislav Glumov <19gvi09@gmail.com>
2024-03-12 14:23:05 +0300

    test husky with lint-staged

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,8 @@
 import "@/styles/globals.css";
 import type {AppProps} from "next/app";
 import "antd/dist/reset.css";
-import {ConfigProvider, Layout} from "antd/lib"
-import {ApolloGraphqlProvider} from "@/shared/graphql"
+import {ConfigProvider} from "antd/lib";
+import {ApolloGraphqlProvider} from "@/shared/graphql";
 
 export default function App({Component, pageProps}: AppProps) {
   return (
